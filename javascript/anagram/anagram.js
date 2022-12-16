3b121a24aa3584546eac467308f49cea9fda5b31
JustWaveThings <justwavethings@proton.me>
2022-12-16 18:12:10 -0500

    Pseudo the comparison of target and input word

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,11 +1,42 @@
-
 export const findAnagrams = (target, candidates) => {
-  let targetLC = target.toLowerCase()
-  let candidatesLC = candidates.map((word) => word.toLowerCase());
+	let targetLC = target.toLowerCase();
+	let candidatesLC = candidates.map((word) => word.toLowerCase());
+	let excludeList = [];
+	let returnList = [];
 
-  candidatesLC.forEach((word) => {
-     if (word === targetLC || word.length !== targetLC.length) {candidatesLC.splice(candidatesLC.indexOf(word), 1)}
-    
-  })
-  return candidatesLC
+	candidatesLC.forEach((word) => {
+		if (word === targetLC) {
+			console.log(`${word} exact match`);
+			excludeList.push(word);
+		}
+	});
+
+	candidatesLC.forEach((word) => {
+		if (word.length !== targetLC.length) {
+			console.log(`${word} not same length`);
+			excludeList.push(word);
+		}
+	});
+
+	candidatesLC.forEach((word, index) => {
+		if (word !== excludeList[index]) {
+			returnList.push(word);
+		}
+	});
+
+	returnList.forEach((word, index) => {
+		let returnListWordArray = [...word];
+		let targetLCArray = [...targetLC];
+		let discardIndex = [];
+		let currentWordIndex = 0;
+
+		// I have a word that has passed a test on length and not direct equality('spam' === 'spam'). I now need to compare one word at a time, each of it's letters to the letters in the target word. If any of the letters do not match, I need to throw the whole word out.
+
+		//I have two arrays, each with the word to compare. I will increment a counter in the outside scope to add the indices to remove from the word array if there's a letter that doesn't match.
+	});
+
+	//	console.log({ excludeList });
+	//	console.log({ candidatesLC });
+
+	return returnList;
 };
