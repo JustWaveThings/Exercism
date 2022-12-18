5de7ec32d5def34aed131892c0f1a1d8d931b77d
JustWaveThings <justwavethings@proton.me>
2022-12-18 15:12:23 -0500

    All tests pass

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,1 +1,18 @@
-export const toRna = () => {};
+export const toRna = (input) => {
+	let inputArray = [...input];
+	let outputArray = [];
+	inputArray.forEach((letter) => {
+		switch (letter) {
+			case 'G':
+				return outputArray.push('C');
+			case 'C':
+				return outputArray.push('G');
+			case 'T':
+				return outputArray.push('A');
+			case 'A':
+				return outputArray.push('U');
+			default:
+		}
+	});
+	return outputArray.join('');
+};
