23b88e5d2e345901b8bab0162ff54d3cd8316782
JustWaveThings <justwavethings@proton.me>
2022-12-23 11:30:37 -0500

    Matching brackets exercise complete

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -1,8 +1,32 @@
-//
-// This is only a SKELETON file for the 'Matching Brackets' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const isPaired = (input) => {
+	let bracketOnlyRegex = /[^(\[)|(\])|(\{)|(\})|(\()|(\))]/g;
+	let bracketsOnly = input.replace(bracketOnlyRegex, '');
+	let bracketsArray = [...bracketsOnly];
+	let openBracketArray = ['(', '[', '{'];
+	let closeBracketArray = [')', ']', '}'];
+	let comparisonArray = [];
 
-export const isPaired = () => {
-  throw new Error('Remove this statement and implement this function');
+	bracketsArray.forEach((bracket) => {
+		if (openBracketArray.includes(bracket)) {
+			comparisonArray.push(bracket);
+		}
+		if (closeBracketArray.includes(bracket)) {
+			// close bracket
+			let lastElementPushed = comparisonArray.pop(); // normal - gives us an opening bracket of same type
+			//now we need to compare line 13's closing bracket to line 14's potentially opening bracket  -- do this by index of the two arrays if the indexes match, then they are a pair. if not, return false
+			//
+			if (
+				openBracketArray.indexOf(lastElementPushed) ===
+				closeBracketArray.indexOf(bracket)
+			) {
+			} else {
+				comparisonArray.push(bracket);
+			}
+		}
+	});
+	if (comparisonArray.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
 };
