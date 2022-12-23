b640528e4a9ef75fc24c85da0d092b9b8957f843
JustWaveThings <justwavethings@proton.me>
2022-12-23 11:31:04 -0500

    initial start for diffie-hellman exercise

---
+++ javascript/diffie-hellman/diffie-hellman.js
@@ -0,0 +1,19 @@
+export class DiffieHellman {
+	constructor(p, g) {
+		throw new Error(
+			'Remove this statement and implement this function'
+		);
+	}
+
+	getPublicKey(privateKey) {
+		throw new Error(
+			'Remove this statement and implement this function'
+		);
+	}
+
+	getSecret(theirPublicKey, myPrivateKey) {
+		throw new Error(
+			'Remove this statement and implement this function'
+		);
+	}
+}
